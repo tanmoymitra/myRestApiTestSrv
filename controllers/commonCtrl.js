1b6ebc1b4e2755af611db198b145aea26dc0e382
Tanmoy Mitra <tanmoymitra@gmail.com>
2017-07-13 21:45:43 +0530

    Middleware updated

--- controllers/commonCtrl.js
+++ controllers/commonCtrl.js
@@ -1,3 +1,22 @@
+const getIP = require('external-ip')();
+const where = require('node-where');
+
 exports.all = function(req, res){
     res.send({"status":"Express API is Working!"})
+};
+
+exports.geolocation = function(req, res){
+    getIP(function (err, ip) {
+        if (err) { throw err; }
+        where.is(ip, function(err, result) {
+            if (err) { throw err; }
+            req.geoip = ip;
+            req.geoLoc = result.attributes;
+            var data = {
+                ip: ip,
+                geo:result.attributes
+            };
+            res.send(JSON.stringify(data));
+        });
+    });
 };
--- routers/apiRouters.js
+++ routers/apiRouters.js
@@ -2,5 +2,6 @@ var express = require("express")
 var router = express.Router();
 
 router.get('/', require('../controllers/commonCtrl').all)
+router.get('/geolocation', require('../controllers/commonCtrl').geolocation)
 
 module.exports = router
