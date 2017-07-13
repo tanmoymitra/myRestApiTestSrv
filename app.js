8ab58399d52f45cdc5550c3ce58eb413b7aec86d
Tanmoy Mitra <tanmoymitra@gmail.com>
2017-07-14 02:38:17 +0530

    Update

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var helmet = require('helmet')
 var cors = require('cors')
 var compression = require('compression')
 var cluster = require('cluster')
+var cache = require('apicache').middleware
+
 
 if (cluster.isMaster) {
     var cpuCount = require('os').cpus().length;
@@ -21,7 +23,7 @@ if (cluster.isMaster) {
 } else {
     var app = express()
     app.set('port', process.env.PORT || 3000)
-
+    app.use(cache('5 minutes'))
     app.use(compression())
     app.use(helmet())
     app.use(cors());
