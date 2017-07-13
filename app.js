ac6e20d20300b74af8944006d97e9f1d5890f00b
Tanmoy Mitra <tanmoymitra@gmail.com>
2017-07-13 21:37:05 +0530

    Initial commit with default files

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use(require('./routers/apiRouters'))
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express API Server Listening on port ' + app.get('port'));
