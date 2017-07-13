9efbc1a927a848aa190b5438280bdba071d6e9f0
Tanmoy Mitra <tanmoymitra@gmail.com>
2017-07-14 00:28:36 +0530

    Update

--- app.js
+++ app.js
@@ -22,9 +22,29 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-// v1 specfic code
+
+// Version # 1.0.0 
 app.use(require('./v1/routers/apiRouters'))
 app.use(require('./v1/middlewares/commonResponse').setResponseHeader)
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    res.render('error', {
+        message: err.message,
+        error: err
+    })
+  })
+}
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    res.render('error', {
+        message: err.message,
+        error: {}
+    })
+})
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express API Server Listening on port ' + app.get('port'));
--- v1/routers/apiRouters.js
+++ v1/routers/apiRouters.js
@@ -2,6 +2,6 @@ var express = require("express")
 var router = express.Router();
 
 router.get('/', require('../controllers/commonCtrl').all)
-router.get('/geolocation', require('../controllers/commonCtrl').geolocation)
+router.get('/api/v1/geolocation', require('../controllers/commonCtrl').geolocation)
 
 module.exports = router
