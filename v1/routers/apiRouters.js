912a18432d565f78ac19c1e6b602c6180d4f79ac
Tanmoy Mitra <tanmoymitra@gmail.com>
2017-07-14 00:35:46 +0530

    Update

--- v1/routers/apiRouters.js
+++ v1/routers/apiRouters.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var router = express.Router();
 
+// All routers
 router.get('/', require('../controllers/commonCtrl').all)
 router.get('/api/v1/geolocation', require('../controllers/commonCtrl').geolocation)
 
