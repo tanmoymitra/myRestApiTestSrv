69c1b77aa2591f2c4e9fe3e540cfa7139448d7ee
Tanmoy Mitra <tanmoymitra@gmail.com>
2017-07-13 21:52:45 +0530

    Initial code

--- controllers/commonCtrl.js
+++ controllers/commonCtrl.js
@@ -1,22 +1,12 @@
 const getIP = require('external-ip')();
 const where = require('node-where');
+var requestIp = require('request-ip');
 
 exports.all = function(req, res){
     res.send({"status":"Express API is Working!"})
 };
 
 exports.geolocation = function(req, res){
-    getIP(function (err, ip) {
-        if (err) { throw err; }
-        where.is(ip, function(err, result) {
-            if (err) { throw err; }
-            req.geoip = ip;
-            req.geoLoc = result.attributes;
-            var data = {
-                ip: ip,
-                geo:result.attributes
-            };
-            res.send(JSON.stringify(data));
-        });
-    });
+    var clientIp = requestIp.getClientIp(req);
+    res.send({"IP":clientIp});
 };
