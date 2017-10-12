5b7f9be0618f29c5e496cf46024c1d35b43e8eea
Tanmoy Mitra <tanmoymitra@gmail.com>
2017-10-13 00:40:49 +0530

    Api.ai implementing

--- v1/controllers/facebookChatBotCtrl.js
+++ v1/controllers/facebookChatBotCtrl.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const apiaiApp = require('apiai')("820fabf8d8574d13ac6cb89dd3db33d7");
 const token = "EAACYfc2K7rcBAFERnp7T3eZAcCGDTMD0iDCGj818wWzhc6BuBCQhVyOVStyDFA9EECh5wFHuOZAmNrsPdOq0hS5Qn8IjZABGX0EUV3tLkeMICd6HxJmPL1x2Kj4WOZB482TYmjNJbJfnSDtMxExeo4k9RTwe8k0NuohDzixjdgZDZD"
 exports.validate = function(req, res){
     if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === "T@nm0yMitr@") {
@@ -47,26 +48,39 @@ function receivedMessage(event) {
 }
 function sendTextMessage(recipientId, messageText) {
     //console.log("sendTextMessage");
-    request({
-        uri: 'https://graph.facebook.com/v2.6/'+recipientId,
-        qs: { fields:'first_name,last_name', access_token: token },
-        method: 'GET'
-      }, function (error, response, body) {
-          console.log(body);
-        if (!error && response.statusCode == 200) {
-            var first_name = JSON.parse(body).first_name;
-            var Last_name = JSON.parse(body).last_name;
-            var messageData = {
-                recipient: {
-                  id: recipientId
-                },
-                message: {
-                  text: "Hello "+first_name+" "+Last_name+", Welcome to My Page" 
-                }
-            };
-            callSendAPI(messageData);
-        }
-    }); 
+    let apiai = apiaiApp.textRequest(messageText, {
+        sessionId: 'tabby_cat' // use any arbitrary id
+    });
+    apiai.on('response', (response) => {
+    // Got a response from api.ai. Let's POST to Facebook Messenger
+        let aiText = response.result.fulfillment.speech;
+        request({
+            uri: 'https://graph.facebook.com/v2.6/'+recipientId,
+            qs: { fields:'first_name,last_name', access_token: token },
+            method: 'GET'
+          }, function (error, response, body) {
+              console.log(body);
+            if (!error && response.statusCode == 200) {
+                var first_name = JSON.parse(body).first_name;
+                var Last_name = JSON.parse(body).last_name;
+                var messageData = {
+                    recipient: {
+                      id: recipientId
+                    },
+                    message: {
+                      text: aiText 
+                    }
+                };
+                callSendAPI(messageData);
+            }
+        }); 
+    });
+
+    apiai.on('error', (error) => {
+        //console.log(error);
+    });
+
+    apiai.end();
 }
 function callSendAPI(messageData) {
     request({
